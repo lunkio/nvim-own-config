c992f43c0dd937b311544dddee43e72dbe00c1f9
lunkio <Sun_coding@outlook.com>
2025-11-22 08:44:48 +0800

    feat: refine clangd config in lsp/clangd.lua

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -9,15 +9,23 @@
 -- src: https://github.com/neovim/nvim-lspconfig/blob/master/lsp/clangd.lua
 ---@type vim.lsp.Config
 return {
-	cmd = { "clangd", "--clang-tidy", "--background-index", "--offset-encoding=utf-8" },
-	filetypes = { "c", "h","hpp", "cpp", "objc", "objcpp", "cuda" },
-	root_markers = {
-		".clangd",
-		".clang-tidy",
-		".clang-format",
-		"compile_commands.json",
-		"compile_flags.txt",
-		"configure.ac", -- AutoTools
-		".git",
-	},
+    cmd = { "clangd", "--clang-tidy", "--background-index", "--offset-encoding=utf-8" },
+    filetypes = { "c", "h", "hpp", "cpp", "objc", "objcpp", "cuda" },
+    root_markers = {
+        ".clangd",
+        ".clang-tidy",
+        ".clang-format",
+        "compile_commands.json",
+        "compile_flags.txt",
+        "configure.ac", -- AutoTools
+        ".git",
+    },
+    capabilities = {
+        textDocument = {
+            completion = {
+                editsNearCursor = true,
+            },
+        },
+        offsetEncoding = { 'utf-8', 'utf-16' },
+    },
 }
